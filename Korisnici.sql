59cb099d27267da14e791349d20941af64f7cb71
Antonio Labinjan <119959820+AntonioLabinjan@users.noreply.github.com>
2024-01-08 13:16:02 +0100

    Update Korisnici.sql

--- Korisnici.sql
+++ Korisnici.sql
@@ -1,29 +1,30 @@
-/*# KORISNICI (autentifikacija/autorizacija)
--- Kreiranje admin korisnika
+# KORISNICI (autentifikacija/autorizacija)
+# OVAKO SMO TESTIRALI KORISNIKE U SQL-u (napravimo ga, dodamo mu prava, oduzmemo mu prava, dropamo ga
+# Kreiranje admin korisnika
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin_password';
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
--- prikaz ovlasti
+# prikaz ovlasti
 SHOW GRANTS FOR 'admin'@'localhost';
--- oduzimanje ovlasti
+# oduzimanje ovlasti
 REVOKE ALL PRIVILEGES ON *.* FROM 'admin'@'localhost';
 FLUSH PRIVILEGES;
--- brisanje korisnika
+# brisanje korisnika
 DROP USER 'admin'@'localhost';
 
 
--- Kreiranje HR korisnika
+# Kreiranje HR korisnika
 CREATE USER 'hr'@'localhost' IDENTIFIED BY 'hr_password';
 GRANT SELECT, INSERT, UPDATE, DELETE ON Policija.Radno_mjesto TO 'hr'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE ON Policija.Odjeli TO 'hr'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE ON Policija.Zaposlenik TO 'hr'@'localhost';
 FLUSH PRIVILEGES;
--- prikaz ovlasti
+# prikaz ovlasti
 SHOW GRANTS FOR 'hr'@'localhost';
--- oduzimanje ovlasti
+# oduzimanje ovlasti
 REVOKE ALL PRIVILEGES ON *.* FROM 'hr'@'localhost';
 FLUSH PRIVILEGES;
--- brisanje korisnika
+# brisanje korisnika
 DROP USER 'hr'@'localhost';
 
 # Napravi korisnika za običnu fizičku osobu koja nije djelatnik policije i ima pristup samo osnovnijim, neklasificiranim tablicama
@@ -41,10 +42,10 @@ FLUSH PRIVILEGES;
 SHOW GRANTS FOR 'fizicka_osoba'@'localhost';
 REVOKE ALL PRIVILEGES ON *.* FROM 'fizicka_osoba'@'localhost';
 FLUSH PRIVILEGES;
--- brisanje korisnika
+# brisanje korisnika
 DROP USER 'fizicka_osoba'@'localhost';
 
-# Napravi korisnika 'detektiv' (ne znan dali je to egzaktan naziv) koji će biti zadužen za prikupljanje dokaza na slučajevima, predmete, sredstva_utvrđivanja_istine i sastavljanje izvještaja
+# Napravi korisnika 'detektiv' (ne znan dali je to egzaktan naziv, ali ok...valjda ni toliko bitno) koji će biti zadužen za prikupljanje dokaza na slučajevima, predmete, sredstva_utvrđivanja_istine i sastavljanje izvještaja
 CREATE USER 'detektiv'@'localhost' IDENTIFIED BY 'detektiv_password';
 GRANT SELECT, INSERT, UPDATE, DELETE ON Policija.Predmet TO 'detektiv'@'localhost';
 GRANT SELECT, INSERT, UPDATE, DELETE ON Policija.Slucaj TO 'detektiv'@'localhost';
